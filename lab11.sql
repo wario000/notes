da77b5f5ab454596d16d93b8114cd3f1c9c11823
wario000 <janesteleydes@gmail.com>
2024-11-18 00:20:36 -0500

    Create lab11.sql

---
+++ lab11.sql
@@ -0,0 +1,25 @@
+SELECT * FROM courses;
+
+SELECT * FROM assignments
+LIMIT 10; 
+
+SELECT count(*) FROM courses;
+
+SELECT min(due_date) FROM assignments;
+
+SELECT *
+FROM  courses
+WHERE course_name LIKE 'Intro%';
+
+SELECT *
+FROM  assignments
+WHERE status != 'Completed'
+ORDER BY due_date;
+
+-- Use this query as a reference for the next step
+SELECT course_id, title, status, due_date
+FROM assignments
+WHERE status != 'Completed'	
+  AND course_id LIKE 'COMM%'
+  AND due_date < '2024-12-31'
+ORDER BY due_date;
